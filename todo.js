8b75641583e2cd8592e1933d7498d923ed160d92
ravi1225 <ravi@excellencetechnologies.in>
2021-01-28 11:50:06 +0530

    done with modal designing

--- todo.js
+++ todo.js
@@ -25,27 +25,24 @@ export default function Todo() {
         <>
         <Modal 
             visible={true}
+            transparent={true}
         >
             <View style={styles.external_View}>
                 <View style={styles.internal_View}>
-                <Text> Add Todo </Text>
+                <Text style={styles.header_text}> Add Todo </Text>
                 <TextInput
-                    placeholder="Add whatever you want to add"
-                    // onChangeText= {textInput}
-                    // value={text}
+                    style={styles.input_Box}
                 />
                 <View style={styles.third_View}>
                     <TouchableOpacity>
-                        <Text>Cancel</Text>
+                        <Text style={styles.text_style}>Cancel</Text>
                     </TouchableOpacity>
 
                     <TouchableOpacity>
-                        <Text>Done</Text>
+                        <Text style={styles.text_style}>Done</Text>
                     </TouchableOpacity>
+                </View>  
                 </View>
-                
-                </View>
-
             </View>
         </Modal>
     </>
@@ -57,24 +54,46 @@ export default function Todo() {
 const styles = StyleSheet.create({
     external_View: {
         // transparent: 'true'
-        borderWidth: 1,
         padding: 5,
-        borderRadius: 10,
-        
-        
+        borderRadius: 20,
     },
 
     internal_View: {
-        borderRadius: 10,
+        borderRadius: 15,
         paddingRight: 5,
         borderWidth: 1,
-        padding: 5,
+        paddingBottom: 15,
+        paddingLeft: 10,
+        paddingTop: 15,
+        paddingRight: 10,
+        borderColor: "lightgray",
+        
+    },
+
+    input_Box: {
+        borderWidth: 1,
+        borderColor: "lightgray",
+        borderRadius: 10,
+        height: 100,
     },
 
     third_View: {
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
+        paddingLeft: 10,
+        paddingRight: 10,
+    },
+
+    header_text: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        paddingBottom: 10,
+    },
+
+    text_style: {
+        color: 'blue',
+        fontSize: 20,
     }
 
   });
